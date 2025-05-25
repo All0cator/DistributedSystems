46fce9f74f266e860490780a91cd44fe5d4a76dd
Nikos <nikostomaras007@gmail.com>
2025-05-25 15:02:12 +0300

    a

--- src/main/java/Primitives/Payloads/ManagerStatePayload.java
+++ src/main/java/Primitives/Payloads/ManagerStatePayload.java
@@ -1,12 +1,14 @@
 package Primitives.Payloads;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Set;
+import Primitives.Store;
 
 public class ManagerStatePayload implements Serializable {
     public int mapID;
     public int numWorkers;
     public Set<String> foodCategories;
     public Set<String> productTypes;
-    public Set<String> storeNames;
+    public ArrayList<Store> stores;
 }
